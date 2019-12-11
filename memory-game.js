f655c82c64dfebfe54223d351076bbc2900e336c
jessicaelee <jessica.e.lee.93@gmail.com>
2019-12-11 01:00:59 -0800

    delete files

---
+++ memory-game.js
@@ -0,0 +1,7 @@
+const cards = document.querySelectorAll(".card");
+
+cards.forEach(card => card.addEventListener("click", flipCard));
+
+function flipCard() {
+    this.classList.toggle("is-flipped");
+}
